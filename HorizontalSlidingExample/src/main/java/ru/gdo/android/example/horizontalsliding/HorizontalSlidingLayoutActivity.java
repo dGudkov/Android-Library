47f3b0716dc083d57bf49874b3e566681cb88caf
Danil Gudkov <danil.gudkov@progforce.com>
2015-07-27 10:24:38 +0300

    Added coder desription

--- HorizontalSlidingExample/src/main/java/ru/gdo/android/example/horizontalsliding/HorizontalSlidingLayoutActivity.java
+++ HorizontalSlidingExample/src/main/java/ru/gdo/android/example/horizontalsliding/HorizontalSlidingLayoutActivity.java
@@ -3,6 +3,12 @@ package ru.gdo.android.example.horizontalsliding;
 import android.app.Activity;
 import android.os.Bundle;
 
+/**
+ * @author Danil Gudkov <d_n_l@mail.ru>
+ * @copyrights Danil Gudkov, 2015
+ * @since 23.07.15.
+ */
+
 public class HorizontalSlidingLayoutActivity extends Activity {
 
     @Override
--- VerticalSlidingExample/src/main/java/ru/gdo/android/example/verticalsliding/VerticalSlidingLayoutActivity.java
+++ VerticalSlidingExample/src/main/java/ru/gdo/android/example/verticalsliding/VerticalSlidingLayoutActivity.java
@@ -3,6 +3,12 @@ package ru.gdo.android.example.verticalsliding;
 import android.app.Activity;
 import android.os.Bundle;
 
+/**
+ * @author Danil Gudkov <d_n_l@mail.ru>
+ * @copyrights Danil Gudkov, 2015
+ * @since 23.07.15.
+ */
+
 public class VerticalSlidingLayoutActivity extends Activity {
 
     @Override
